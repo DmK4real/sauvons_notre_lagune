c39a5117ddeb3e3a6fbd155136647fed09cbed8c
DOMINIQUE KOUAKOU <dominiquekouakou2@gmail.com>
2025-01-27 19:08:11 +0000

    Ajout ou mise à jour du fichier backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -63,4 +63,6 @@ if __name__ == '__main__':
     print("Chemin absolu pour static :", os.path.abspath('static'))
     
     # Lancement de l'application Flask
-    app.run(host='0.0.0.0', port=5000)
+    
+    app.run(debug=True, host='0.0.0.0')
+
